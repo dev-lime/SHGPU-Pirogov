24a6a353c06a34c695fa3afbeb8dc809b60fbaa4
l1me <102348214+dev-lime@users.noreply.github.com>
2025-09-15 13:14:14 +0500

    Fix auth

--- linux/tk/models/UserModel.php
+++ linux/tk/models/UserModel.php
@@ -10,21 +10,32 @@ class UserModel extends BaseModel
     {
         $con = getDBConnection();
         $sql = "SELECT u.*, r.role_name, r.permissions 
-                FROM users u 
-                LEFT JOIN roles r ON u.role_id = r.role_id 
-                WHERE u.username = $1";
+            FROM users u 
+            LEFT JOIN roles r ON u.role_id = r.role_id 
+            WHERE u.username = $1";
+
         $result = pg_query_params($con, $sql, [$username]);
-        return pg_fetch_assoc($result);
+
+        if (!$result) {
+            error_log("Query failed: " . pg_last_error($con));
+            return false;
+        }
+
+        $user = pg_fetch_assoc($result);
+
+        if ($user && isset($user['permissions'])) {
+            $user['permissions'] = trim($user['permissions'], '{}');
+            $user['permissions'] = explode(',', $user['permissions']);
+        }
+
+        return $user;
     }
 
     public static function verifyPassword($password, $hash)
     {
-        // Добавим отладочную информацию
-        error_log("Password: " . $password);
-        error_log("Hash: " . $hash);
-        error_log("Verification result: " . (password_verify($password, $hash) ? 'true' : 'false'));
+        $cleanHash = trim($hash, '{}');
 
-        return password_verify($password, $hash);
+        return password_verify($password, $cleanHash);
     }
 
     public static function hashPassword($password)
